74a1737f3b54f6c7db6db6788d1c7b525180c883
Pedrodmm63 <pedrodmm63@educastur.es>
2024-03-06 10:00:27 +0100

    Ayuda en planes y usuarios

--- src/main/java/com/spring/start/plan/PlanController.java
+++ src/main/java/com/spring/start/plan/PlanController.java
@@ -1,7 +1,9 @@
 package com.spring.start.plan;
 
+import java.util.HashSet;
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -15,7 +17,8 @@ import com.spring.start.dietas.Dieta;
 import com.spring.start.dietas.DietaDAO;
 import com.spring.start.entrenamientos.Entrenamiento;
 import com.spring.start.entrenamientos.EntrenamientoDAO;
-
+import com.spring.start.usuarios.Usuario;
+import com.spring.start.usuarios.UsuarioDAO;
 
 @Controller
 public class PlanController {
@@ -26,31 +29,34 @@ public class PlanController {
 	PlanDAO planDAO;
 	@Autowired
 	EntrenamientoDAO entrenamientoDAO;
-	
+	@Autowired
+	UsuarioDAO usuarioDAO;
+
 	@GetMapping("/plan")
 	public ModelAndView planes() {
 		ModelAndView model = new ModelAndView();
 		model.setViewName("planes");
-		
+
 		Plan plan = new Plan();
 		model.addObject("plan", plan);
-		
+
 		List<Plan> planes = (List<Plan>) planDAO.findAll();
 		model.addObject("planes", planes);
 		return model;
 	}
+
 	@GetMapping("/plan/{id}")
-	public ModelAndView plan(@PathVariable long id){
-		
+	public ModelAndView plan(@PathVariable long id) {
+
 		Plan plan = planDAO.findById(id).get();
-		
+
 		ModelAndView model = new ModelAndView();
 		model.setViewName("plan");
 		model.addObject("plan", plan);
-		
+
 		return model;
 	}
-	
+
 	@GetMapping("/plan/add")
 	public ModelAndView addPlan() {
 
@@ -59,52 +65,64 @@ public class PlanController {
 		model.setViewName("formPlan");
 		model.addObject("plan", plan);
 		model.addObject("dietas", dietaDAO.findAll());
+		model.addObject("usuarios", usuarioDAO.findAll());
 		model.addObject("entrenamientos", entrenamientoDAO.findAll());
-
 
 		return model;
 	}
+
 	@PostMapping("/plan/save")
 	public ModelAndView formTutoria(@ModelAttribute Plan plan) {
 
-	    if (plan.getDieta() != null) {
-	        Optional<Dieta> optionalDieta = dietaDAO.findById(plan.getDieta().getId());
-	        if (optionalDieta.isPresent()) {
-	            Dieta dieta = optionalDieta.get();
-	            // Aquí se asume que ya existe una relación entre plan y dieta y que solo se necesita actualizar la referencia.
-	            plan.setDieta(dieta);
-	            // Añadir el plan a la lista de planes de la dieta si no está presente
-	            if (!dieta.getPlanes().contains(plan)) {
-	                dieta.getPlanes().add(plan);
-	            }
-	            // No es necesario guardar explícitamente la dieta aquí si la relación entre Plan y Dieta está configurada con cascade en el mapeo JPA
-	        }
-	    }
+		if (plan.getDieta() != null) {
+			Optional<Dieta> optionalDieta = dietaDAO.findById(plan.getDieta().getId());
+			if (optionalDieta.isPresent()) {
+				Dieta dieta = optionalDieta.get();
+				plan.setDieta(dieta);
+				if (!dieta.getPlanes().contains(plan)) {
+					dieta.getPlanes().add(plan);
+				}
+			}
+		}
 
-	    if (plan.getEntrenamiento() != null) {
-	        Optional<Entrenamiento> optionalEntrenamiento = entrenamientoDAO.findById(plan.getEntrenamiento().getId());
-	        if (optionalEntrenamiento.isPresent()) {
-	            Entrenamiento entrenamiento = optionalEntrenamiento.get();
-	            // Si Entrenamiento maneja una lista de Planes similar a Dieta, realizar la lógica adecuada aquí
-	            plan.setEntrenamiento(entrenamiento);
-	            
-	            if (!entrenamiento.getPlanes().contains(plan)) {
-	                entrenamiento.getPlanes().add(plan);
-	            }
-	        }
-	    }
+		if (plan.getEntrenamiento() != null) {
+			Optional<Entrenamiento> optionalEntrenamiento = entrenamientoDAO.findById(plan.getEntrenamiento().getId());
+			if (optionalEntrenamiento.isPresent()) {
+				Entrenamiento entrenamiento = optionalEntrenamiento.get();
+				plan.setEntrenamiento(entrenamiento);
 
-	    planDAO.save(plan);
+				if (!entrenamiento.getPlanes().contains(plan)) {
+					entrenamiento.getPlanes().add(plan);
+				}
+			}
+		}
+		if (!plan.getUsuarios().isEmpty()) {
+		    Set<Usuario> usuariosActualizados = new HashSet<Usuario>();
+		    for (Usuario usuario : plan.getUsuarios()) {
+		        Optional<Usuario> optionalUsuario = usuarioDAO.findById(usuario.getId());
+		        if (optionalUsuario.isPresent()) {
+		            Usuario usuarioExistente = optionalUsuario.get();
+		            usuariosActualizados.add(usuarioExistente);
+		            
+		            if (!usuarioExistente.getPlanes().contains(plan)) {
+		                usuarioExistente.getPlanes().add(plan);
+		            }
+		        }
+		    }
+		    plan.setUsuarios(usuariosActualizados);
+		}
 
-	    ModelAndView model = new ModelAndView();
-	    model.addObject("planNuevo", plan);
-	    model.setViewName("redirect:/plan/nuevo/" + plan.getId());
+		planDAO.save(plan);
 
-	    return model;
+		ModelAndView model = new ModelAndView();
+		model.addObject("planNuevo", plan);
+		model.setViewName("redirect:/plan/nuevo/" + plan.getId());
+
+		return model;
 	}
 
 	@GetMapping("plan/nuevo/{id}")
-	public ModelAndView popupNuevoPlan(@PathVariable long id) {
+	public ModelAndView NuevoPlan(@PathVariable long id) {
 
 		Plan plan = new Plan();
 		ModelAndView model = new ModelAndView();
@@ -118,8 +136,97 @@ public class PlanController {
 		model.addObject("planes", planes);
 		model.addObject("planNuevo", planNuevo);
 		model.addObject("entrenamiento", entrenamientoDAO.findAll());
+		model.addObject("usuarios", usuarioDAO.findAll());
 		model.setViewName("planes");
 
 		return model;
 	}
+
+	@GetMapping("/plan/edit/{id}")
+	public ModelAndView editPlan(@PathVariable long id) {
+
+		ModelAndView model = new ModelAndView();
+
+		Optional<Plan> planazo = planDAO.findById(id);
+		if (planazo.isPresent()) {
+
+			model.addObject("plan", planazo.get());
+			model.addObject("dietas", dietaDAO.findAll());
+
+			model.addObject("entrenamientos", entrenamientoDAO.findAll());
+			model.addObject("usuarios", usuarioDAO.findAll());
+
+			model.setViewName("formPlan");
+		} else
+			model.setViewName("redirect:/plan");
+
+		return model;
+	}
+
+	@GetMapping("/plan/del/{id}")
+	public ModelAndView delPlan(@PathVariable long id) {
+
+		planDAO.deleteById(id);
+
+		ModelAndView model = new ModelAndView();
+		model.setViewName("redirect:/plan");
+
+		return model;
+	}
+
+	@GetMapping("/plan/dieta/del/{idPlan}")
+	public ModelAndView eliminarPlanDieta(@PathVariable long idPlan) {
+
+		Optional<Plan> plan = planDAO.findById(idPlan);
+		if (plan.isPresent()) {
+
+			Plan planazo = plan.get();
+			Dieta dieta = planazo.getDieta();
+			planazo.setDieta(null);
+			dieta.setPlanes(null);
+			planDAO.save(planazo);
+		}
+
+		ModelAndView model = new ModelAndView();
+		model.setViewName("redirect:/plan");
+
+		return model;
+	}
+
+	@GetMapping("/plan/entrenamiento/del/{idPlan}")
+	public ModelAndView eliminarPlanEntrenamiento(@PathVariable long idPlan) {
+
+		Optional<Plan> plan = planDAO.findById(idPlan);
+		if (plan.isPresent()) {
+
+			Plan planazo = plan.get();
+			Entrenamiento entrenamiento = planazo.getEntrenamiento();
+			planazo.setEntrenamiento(null);
+			entrenamiento.setPlanes(null);
+			planDAO.save(planazo);
+		}
+
+		ModelAndView model = new ModelAndView();
+		model.setViewName("redirect:/plan");
+
+		return model;
+	}
+	/*@GetMapping("/plan/entrenamiento/del/{idPlan}")
+	public ModelAndView eliminarPlanUsuario(@PathVariable long idPlan) {
+
+		Optional<Plan> plan = planDAO.findById(idPlan);
+		if (plan.isPresent()) {
+
+			Plan planazo = plan.get();
+			Usuario usuario = (Usuario) planazo.getUsuarios();
+			planazo.setEntrenamiento(null);
+			usuario.setPlanes(null);
+			planDAO.save(planazo);
+		}
+
+		ModelAndView model = new ModelAndView();
+		model.setViewName("redirect:/plan");
+
+		return model;
+	}*/
 }
---
+++ src/main/java/com/spring/start/usuarios/UsuarioController.java
@@ -0,0 +1,8 @@
+package com.spring.start.usuarios;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class UsuarioController {
+
+}
